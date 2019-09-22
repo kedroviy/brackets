7fb7035dbb3a57c0034c1ae4b1d1e0b6e6972a59
Aleksey <kedraja@gmail.com>
2019-09-22 22:35:41 +0300

    passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a='';
+let b='';
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (str[i]==bracketsConfig[j][0] && str[i+1]==bracketsConfig[j][1]) {
+                    a=str.substr(0,i);
+                    b=str.substr(i+2,str.length);
+                str=a+b;
+                i=i-2;
+                }
+            }
+        }
+    if(str==''){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
